e7e9d3f14d9cc7b05c41003d5997dc4d9c3dca25
Ilya <dadast@outlook.com>
2019-12-22 22:02:18 +1000

    end

---
+++ assets/js/donate.js
@@ -0,0 +1,64 @@
+
+
+function init() {
+    btnDonate = document.getElementsByClassName('donate__method__btn');
+    for (var i = 0; i < btnDonate.length; i++) {
+        btnDonate[i].onclick = changeMethod;
+        addOrderId();
+    }
+
+}
+
+
+var formValue = ['formInputLabel', 'formInputTargets'];
+var chenge = Math.floor(Math.random() * 1000) + 100;
+
+
+
+function addOrderId() {
+    var echName;
+    for (var i = 0; i < formValue.length; i++) {
+        echName = document.getElementById(formValue[i]);
+        echName.value = chenge;
+    }
+}
+
+function changeMethod(eventObj) {
+
+    if (!eventObj.target.classList.contains('donate__method__btn--triangle') && !eventObj.target.parentNode.classList.contains('donate__method__btn--triangle')) {
+
+        for (var i = 0; i < btnDonate.length; i++) {
+            btnDonate[i].classList.remove('donate__method__btn--triangle')
+        }
+        chageDonate()
+    }
+    if (eventObj.target.tagName != 'SPAN') {
+        eventObj.target.classList.add('donate__method__btn--triangle');
+
+    } else {
+        eventObj.target.parentNode.classList.add("donate__method__btn--triangle");
+    }
+}
+
+function chageDonate() {
+    document.getElementById('donateBtc').classList.toggle('donate__displayOn');
+    document.getElementById('donateYd').classList.toggle('donate__displayOn')
+}
+
+
+
+
+
+function btCopy() {
+    var btCopyBtn = document.getElementById('donateBtc');
+    btCopyBtn.classList.toggle('donate__btc--copy');
+
+
+
+    setTimeout(function () {
+        btCopyBtn.classList.toggle('donate__btc--copy');
+    }, 800);
+}
+
+
+init();
